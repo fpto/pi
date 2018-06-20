d59ddfb9454aa001d4cc92ab5777f9115fd35525
fpto <fabricio.puerto@outlook.com>
2018-06-19 18:08:54 -0600

    Added program for making coffee.

---
+++ make-me-coffee.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import schedule
+
+GPIO.setmode(GPIO.BCM)
+
+# GPIO | Relay
+#--------------
+# 26     01
+# 19     02
+# 13     03
+# 06     04
+# 12     05
+# 16     06
+# 20     07
+# 21     08
+
+# initiate list with pin gpio pin numbers
+
+i = 26
+
+
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, GPIO.HIGH)
+
+# Sleep time variables
+
+brewingTime = 10
+
+# MAIN LOOP =====
+# ===============
+def job():
+    try:
+        GPIO.output(i, GPIO.HIGH)
+        time.sleep(brewingTime);
+        GPIO.output(i, GPIO.LOW)
+
+    except KeyboardInterrupt:
+        print " Quit"
+        GPIO.cleanup()
+
+schedule.every().minute.do(job)
+
+while True:
+    schedule.run_pending()
